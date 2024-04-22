763e58c7c159df197109a8789b836756aa0846e7
Aetreas <anthonycaruso157@gmail.com>
2024-04-22 16:54:22 -0400

    Code Comments (Virtual Mouse)

--- Assets/Scripts/UI/VirtualMouseUI.cs
+++ Assets/Scripts/UI/VirtualMouseUI.cs
@@ -23,15 +23,21 @@ public class VirtualMouseUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //actually scales the virtual mouse such that it doesnt break on non-1080p resolutions/scaling
         transform.localScale = Vector3.one * (1f / canvasRectTransform.localScale.x);
         transform.SetAsLastSibling();
+
+
     }
 
     private void LateUpdate()
     {
+        //keeps virtual cursor in screen bounds
         Vector2 virtualMousePosition = virtualMouseInput.virtualMouse.position.value;
         virtualMousePosition.x = Mathf.Clamp(virtualMousePosition.x, 0f, Screen.width);
         virtualMousePosition.y = Mathf.Clamp(virtualMousePosition.y, 0f, Screen.height);
         InputState.Change(virtualMouseInput.virtualMouse.position, virtualMousePosition);
+
+        
     }
 }
